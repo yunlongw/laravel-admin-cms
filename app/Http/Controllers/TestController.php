d720264aa0697e2694fcca3a820776519243caff
yunlongw <wyl0418@gmail.com>
2019-01-31 15:11:20 +0800

    测试控制器*

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -19,10 +19,19 @@ class TestController extends Container
 {
     public function index()
     {
-//        event(new MessageEvent());
-//        event(new NotificationsEvent());
-//        event(new UserLogin());
-//        event(new RssCreatedEvent());
+        echo "这是一个测试控制器";
+    }
+
+    public function socketio()
+    {
+        event(new MessageEvent());
+        event(new NotificationsEvent());
+        event(new UserLogin());
+        event(new RssCreatedEvent());
+    }
+
+    public function sentry()
+    {
         throw new \Exception('sentry 错误测试');
     }
 }
